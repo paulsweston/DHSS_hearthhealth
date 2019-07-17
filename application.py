4b15d141606f82af2b92c238d2cdd7c0f3664563
xLasercut <tianxun.wang2004@gmail.com>
2019-07-17 12:16:27 +0100

    added test code for beanstalk to s3

--- application.py
+++ application.py
@@ -1,5 +1,8 @@
 from flask import Flask, jsonify, render_template, request
 import linecache
+import boto3
+import pandas as pd
+import io
 
 application = Flask(__name__)
 
@@ -17,7 +20,7 @@ def basicchat():
 def chat():
 
     qno = int(request.form.get("qno"))
-    question = request.form.get("question") 
+    question = request.form.get("question")
     message = request.form.get("message")
 
     # Add data to history
@@ -27,11 +30,14 @@ def chat():
     response = linecache.getline('chatq.txt', qno)
     print(response)
     response = response.rstrip('\n')
-        
-    if len(response) < 1:
-        response = "Click <a href='/report'>here</a> to view your report ready for your consulation!"
 
-    return jsonify({"response": response})
+    s3 = boto3.resource('s3')
+
+    obj = s3.Object('hearth-health-report', 'test.csv')
+    report = obj.get()['body'].read()
+    regex_data = pd.read_csv(io.BytesIO(report), header=0, delimiter=",", low_memory=False)
+
+    return jsonify({"response": regex_data.head(10)})
 
 @application.route("/report")
 def report():
